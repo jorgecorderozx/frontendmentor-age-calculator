8453c8c0d9f6d28b252e057e0fa5524c1cc931c6
Jorge Cordero <jorgecorderozx@gmail.com>
2024-07-17 18:19:32 -0400

    HTML inputs error messaged added New error messages styled, some relative measures turned into absolute isEmpty function improved Program flow reestructured errorDisplay function created

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
-function isEmpty(event){
-    if(event.target.value == "") return true;
-    else return false;
+let inputs = document.querySelectorAll("input");
+let errors = document.getElementsByClassName("errorMessage");
+let arrowButton = document.querySelector("button");
+let monthsAndDays = [];
+
+for (month = 1; month <= 12; month++) {
+    if (month < 8) monthsAndDays.push({ month: month, days: (month % 2 != 0) ? 31 : 30 });
+    else monthsAndDays.push({ month: month, days: (month % 2 == 0) ? 31 : 30 });
+}
+monthsAndDays[1].days = 28;
+
+function isEmpty() {
+    for (input = 0; input < inputs.length; input++) {
+        (inputs[input].value === "") ? errorDisplay(true) : errorDisplay(false); 
+    }
 }
 
-function isLeapYear(year){
+function errorDisplay(bool) {
+    if (bool === true) {
+        inputs[input].style.borderColor = "var(--light-red)";
+        errors[input].style.visibility = "visible";
+    }
+    else {
+        inputs[input].style.borderColor = "var(--light-grey)";
+        errors[input].style.visibility = "hidden";
+    }
+}
+
+function isLeapYear(year) {
     return ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
 }
 
-let monthsAndDays = [];
-
-for(month = 1; month <= 12; month++){
-    if(month < 8) monthsAndDays.push({month: month, days: (month % 2 != 0) ? 31 : 30});
-    else monthsAndDays.push({month: month, days: (month % 2 == 0) ? 31 : 30});
-}
-
-monthsAndDays[1].days = 28;
-
-
-
-
+arrowButton.addEventListener("click", isEmpty)
