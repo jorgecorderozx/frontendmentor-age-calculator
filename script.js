f56292a5ba9f5186adbd7808db851ff194a3ea8f
Jorge Cordero <jorgecorderozx@gmail.com>
2024-08-04 19:46:56 -0400

    script file reestructured input type changed from text to number

--- script.js
+++ script.js
@@ -1,36 +1,67 @@
-let inputs = document.querySelectorAll("input");
-let errors = document.getElementsByClassName("errorMessage");
-let arrowButton = document.querySelector("button");
+const inputs = document.querySelectorAll("input");
+const errors = document.querySelectorAll(".errorMessage");
+const submitButton = document.querySelector("button");
+
+
+let today, todayDate, todayMonth, todayYear;
+
+let dayVal, monthVal, yearVal;
+
+let valid = false;
 
 function isEmpty() {
-    for (input = 0; input < inputs.length; input++) {
-        (inputs[input].value === "") ? errorDisplay(true) : errorDisplay(false);
-        errors[input].textContent = "This field is required";
-    }
+    inputs.forEach((input, index) => {
+        input.value === "" ? errorDisplay(true, index) : errorDisplay(false, index);
+    })
+        (inputs[0].value === "" || inputs[1].value === "" || inputs[2].value === "") ? valid = false : valid = true;
 }
 
-function errorDisplay(bool) {
-    if (bool === true) {
-        inputs[input].style.borderColor = "var(--light-red)";
-        errors[input].style.visibility = "visible";
+function errorDisplay(bool, index, message = "This field is required") {
+    if (bool) {
+        inputs[index].style.borderColor = "var(--light-red)";
+        errors[index].style.visibility = "visible";
+        errors[index].textContent = message;
     }
     else {
-        inputs[input].style.borderColor = "var(--light-grey)";
-        errors[input].style.visibility = "hidden";
+        inputs[index].style.borderColor = "var(--light-grey)";
+        errors[index].style.visibility = "hidden";
     }
 }
 
-function dateValidation(){
+function isLeapYear(year) {
+    return (year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0))
+}
+
+function dateValidation() {
+    today = new Date();
+    todayDate = today.getDate();
+    todayMonth = today.getMonth();
+    todayYear = today.getFullYear();
+    dayVal = inputs[0].value;
+    monthVal = inputs[1].value;
+    yearVal = inputs[2].value;
+    let fieldName;
+    inputs.forEach((input, index)=>{
+        switch(index){
+            case 0:
+                fieldName = "day";
+                break;
+            case 1:
+                fieldName = "month";
+                break;
+            case 2:
+                fieldName = "year";
+                break;
+        }
+        ((input.value <= 0 && input.value !== "") || (input.value > 12 && index === 1) || (input.value > 31 && index === 0)) ? errorDisplay(true, index, `Must be a valid ${fieldName}`) : errorDisplay(false, index);
     
+    })
+    if(yearVal > todayYear) errorDisplay(true, 2, "Must be in the past");
+    else if(yearVal > 0) errorDisplay(false, 2);
+
+
 }
 
-function ageCalculation(){
-    let today = new Date();
-    let birthDate = new Date(parseInt(inputs[2].value), parseInt(inputs[1].value), parseInt(inputs[0].value));
-
-}
-
-arrowButton.addEventListener("click", () =>{
-    isEmpty()
-    ageCalculation()
+inputs.forEach((input) => {
+    input.addEventListener("input", dateValidation)
 })
